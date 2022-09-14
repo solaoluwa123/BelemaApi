d0d824fc6e96f760b769b8a7d589d36aa87dde49
akintolafemi <michealakintola106@yahoo.com>
2022-09-14 20:41:40 -0100

    added endpoint to get terminals

---
+++ src/main/java/com/transgate/api/app/services/TerminalsService.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.transgate.api.app.services;
+
+import com.transgate.api.interfaces.TerminalsInterface;
+import com.transgate.api.models.NetworkResponse;
+import com.transgate.api.models.TerminalModel;
+import com.transgate.api.util.ResponseManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Makintola
+ */
+@Service
+public class TerminalsService implements TerminalsInterface {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    ResponseManager responseManager = new ResponseManager();
+    
+    @Override
+    public ResponseEntity Get() {
+        NetworkResponse networkResponse = new NetworkResponse();
+        try {
+            String SQL;
+            List<TerminalModel> terminals;
+            SQL = "SELECT * FROM sparkpay.terminals ORDER BY id DESC";
+            terminals = jdbcTemplate.query(SQL, new RoutesMapper());
+            SQL = "SELECT MIN(date_time) from sparkpay.terminals";
+            String minDate = jdbcTemplate.queryForObject(SQL, String.class);
+            SQL = "SELECT MAX(date_time) from sparkpay.terminals";
+            String maxDate = jdbcTemplate.queryForObject(SQL, String.class);
+            String meta = "{\"minDate\": \"" + minDate + "\", \"maxDate\": \"" + maxDate + "\"}";
+           
+            networkResponse.setCode(200);
+            networkResponse.setStatus("success");
+            networkResponse.setMessage("All Terminals");
+            networkResponse.setData((ArrayList) terminals);
+            networkResponse.setMeta(meta);
+            
+            return responseManager.ResponseOk(networkResponse);
+        } catch (DataAccessException ex) {
+            System.out.println("error>>>>" + ex.getMessage());
+            return responseManager.ResponseInternalServerError();
+        }
+    }
+    
+    class RoutesMapper implements RowMapper<TerminalModel> {
+
+        @Override
+        public TerminalModel mapRow(ResultSet rs, int arg1) throws SQLException {
+            TerminalModel terminal = new TerminalModel();
+            terminal.setId(rs.getInt("id"));
+            terminal.setTerminal_id(rs.getString("terminal_id"));
+            terminal.setMerchant_id(rs.getString("merchant_id"));
+            terminal.setMerchant_name(rs.getString("merchant_name"));
+            terminal.setRoute_mode(rs.getString("route_mode"));
+            terminal.setAcquiring_institution_id(rs.getString("acquiring_institution_id"));
+            terminal.setAcquiring_institution_name(rs.getString("acquiring_institution_name"));
+            terminal.setCbn_bank_code(rs.getString("cbn_bank_code"));
+            terminal.setTerminal_type(rs.getString("terminal_type"));
+            terminal.setDate_time(rs.getString("date_time"));
+            terminal.setDelete_flag(rs.getInt("delete_flag"));
+            terminal.setEdit_flag(rs.getInt("edit_flag"));
+            terminal.setCreate_flag(rs.getInt("create_flag"));
+            return terminal;
+        }
+    }
+}
---
+++ src/main/java/com/transgate/api/models/TerminalModel.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.transgate.api.models;
+
+/**
+ *
+ * @author Makintola
+ */
+public class TerminalModel {
+    String terminal_id, merchant_id, merchant_name, route_mode;
+    String acquiring_institution_id, acquiring_institution_name, cbn_bank_code;
+    String terminal_type, date_time;
+    int id, edit_flag, delete_flag, create_flag;
+
+    public String getTerminal_id() {
+        return terminal_id;
+    }
+
+    public void setTerminal_id(String terminal_id) {
+        this.terminal_id = terminal_id;
+    }
+
+    public String getMerchant_id() {
+        return merchant_id;
+    }
+
+    public void setMerchant_id(String merchant_id) {
+        this.merchant_id = merchant_id;
+    }
+
+    public String getMerchant_name() {
+        return merchant_name;
+    }
+
+    public void setMerchant_name(String merchant_name) {
+        this.merchant_name = merchant_name;
+    }
+
+    public String getRoute_mode() {
+        return route_mode;
+    }
+
+    public void setRoute_mode(String route_mode) {
+        this.route_mode = route_mode;
+    }
+
+    public String getAcquiring_institution_id() {
+        return acquiring_institution_id;
+    }
+
+    public void setAcquiring_institution_id(String acquiring_institution_id) {
+        this.acquiring_institution_id = acquiring_institution_id;
+    }
+
+    public String getAcquiring_institution_name() {
+        return acquiring_institution_name;
+    }
+
+    public void setAcquiring_institution_name(String acquiring_institution_name) {
+        this.acquiring_institution_name = acquiring_institution_name;
+    }
+
+    public String getCbn_bank_code() {
+        return cbn_bank_code;
+    }
+
+    public void setCbn_bank_code(String cbn_bank_code) {
+        this.cbn_bank_code = cbn_bank_code;
+    }
+
+    public String getTerminal_type() {
+        return terminal_type;
+    }
+
+    public void setTerminal_type(String terminal_type) {
+        this.terminal_type = terminal_type;
+    }
+
+    public String getDate_time() {
+        return date_time;
+    }
+
+    public void setDate_time(String date_time) {
+        this.date_time = date_time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEdit_flag() {
+        return edit_flag;
+    }
+
+    public void setEdit_flag(int edit_flag) {
+        this.edit_flag = edit_flag;
+    }
+
+    public int getDelete_flag() {
+        return delete_flag;
+    }
+
+    public void setDelete_flag(int delete_flag) {
+        this.delete_flag = delete_flag;
+    }
+
+    public int getCreate_flag() {
+        return create_flag;
+    }
+
+    public void setCreate_flag(int create_flag) {
+        this.create_flag = create_flag;
+    }
+    
+}
